c0606a72505182a82235444c57813a208ff02f38
Duong <ntduong071200@gmail.com>
2025-05-30 12:23:24 +0700

    Enable associate public ip

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -61,10 +61,11 @@ resource "aws_security_group" "jenkins_sg" {
 resource "aws_instance" "jenkins" {
   key_name = var.jenkin_key_name
 
-  ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t3.medium"
-  subnet_id              = module.network.public_subnets[0]
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = "t3.medium"
+  subnet_id                   = module.network.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
+  associate_public_ip_address = true
 
   user_data = file("init-jenkins.sh")
 
